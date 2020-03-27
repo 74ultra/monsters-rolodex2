3015e8b75acd40812e4361d20d1868b1b6145fee
74ultra <r.barnhill@protonmail.com>
2020-03-27 15:37:18 -0700

    Updates

---
+++ precache-manifest.963a1a951794f2146e8d27b32c8d302d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08bef75b546a44b5e743a4ec0f8cba4b",
+    "url": "/monsters-rolodex2/index.html"
+  },
+  {
+    "revision": "b01c168d0c4ae3cb8216",
+    "url": "/monsters-rolodex2/static/css/main.8829e476.chunk.css"
+  },
+  {
+    "revision": "8a6eda421bc5765c55c2",
+    "url": "/monsters-rolodex2/static/js/2.bc65f737.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex2/static/js/2.bc65f737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b01c168d0c4ae3cb8216",
+    "url": "/monsters-rolodex2/static/js/main.4d19d143.chunk.js"
+  },
+  {
+    "revision": "cba7e818a2411464cee6",
+    "url": "/monsters-rolodex2/static/js/runtime-main.61f685c3.js"
+  }
+]);
